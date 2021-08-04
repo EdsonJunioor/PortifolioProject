fe5f7c68218d72d6d6f27e099e6987bab4e9b8ea
Edson Junior <edson.junior@itixti.com.br>
2021-08-03 23:54:15 -0300

    Inserção de texto e ferramentas usadas no projeto

--- FrontEnd/Portifolio/src/app/Components/Usuario/Usuario/Usuario.component.ts
+++ FrontEnd/Portifolio/src/app/Components/Usuario/Usuario/Usuario.component.ts
@@ -14,9 +14,12 @@ export class UsuarioComponent implements OnInit {
   public usuarios: Usuario[] = [] as Usuario[];
   public usuario: Usuario = new Usuario;
 
+  public textoApresentacao = [
+    { apresentacao: 'Olá me chamo Edson, moro em Sorocaba-SP e atualmente estou cursando o 5º semestre no curso de Análise e Desenvolvimento de Sistemas na universidade ESAMC Sorocaba (no momento chamada ATHON). Sou estagiário a 1 ano 2 meses na ITIX TI onde tive a oportunidade de estagiar 5 meses na equipe de desenvolvimento de software que é meu foco. Criei esse site com intenção de aprimorar e meu conhecimento em programação, ferramentas, conceitos e claro me divertir fazendo o que eu gosto.'},
+    { ferramentas:  'ASP.NET - C# - Angular 12 - SQLServer'},
+  ];
+
   constructor(private usuarioService: UsuarioService, private route: ActivatedRoute) { }
-  
-  public id!: number;
 
   ngOnInit() {
     this.usuarioById(1);    //Aqui passei um id fixo para peger o meu usuario do banco
@@ -32,7 +35,7 @@ export class UsuarioComponent implements OnInit {
       console.error('Erro ao carregar usuarios: ' + erro);
     }
   }
-
+  
   usuarioById(id: number) {
     this.usuarioService.getById(id).subscribe(
       (usuario: Usuario) =>{
